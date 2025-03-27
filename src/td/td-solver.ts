418e708058c31c12e60f19d7520b0097975b38b6
Flavio Turibio <fturibio@squeezestudio.com>
2025-03-27 19:53:35 -0400

    trigger

--- src/td/td-solver.ts
+++ src/td/td-solver.ts
@@ -2,7 +2,7 @@ import { Utils } from 'recurrent-js';
 
 import { Solver, TDEnv, TDOpt } from './../.';
 
-// Trigger 3
+// Trigger
 
 export class TDSolver extends Solver {
   protected readonly alpha: number;
